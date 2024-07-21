69f4f6719b8a9473a726a3b95cd213cfc9ba84fb
louis fortrie <louis.fortrie4@gmail.com>
2024-07-21 23:56:33 +0200

    feat: adding user route :sparkles:

--- src/server_manager.ts
+++ src/server_manager.ts
@@ -6,8 +6,17 @@ import { DefaultErrorHandler } from "./middleware/error-handler.middleware";
 import { RegisterRoutes } from "./routes/routes";
 import { Log } from "./utility/Logging/Log";
 import { requestLogMiddleware } from "./utility/Logging/log.middleware";
+import mysql from "mysql2/promise";
+
+const dbConfig = {
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME,
+};
 
 export const StartServer = async () => {
+  const pool = mysql.createPool(dbConfig);
   const PORT = process.env.PORT || 5055;
 
   const app = Express();
@@ -40,6 +49,11 @@ export const StartServer = async () => {
       platform: platform(),
       type: type(),
     });
+  });
+
+  app.get("/user", async (req: any, res: any) => {
+    const [rows] = await pool.query("SELECT * FROM users");
+    res.json(rows);
   });
 
   return new Promise<Server>((resolve) => {
